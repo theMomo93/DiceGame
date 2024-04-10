77a45fdca5cbcd9e8407bfc389c035443035c11e
theMomo93 <mkaminska93@gmail.com>
2024-04-10 11:00:28 +0200

    delay aimation

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var guestPlayer = Math.floor((Math.random() * 6)+1); //1-6
+var guestPlayer = Math.floor((Math.random() * 6) + 1); //1-6
 var randomDiceImage = "dice" + guestPlayer + ".png"; //dice1.png - dice6.png
 var randomImageSource = "dice/" + randomDiceImage; //images/dice1.png - images/dice6.png
 var image1 = document.querySelectorAll("img")[0];
@@ -28,16 +28,16 @@ document.getElementById("button").addEventListener("click", function() {
             document.querySelectorAll(".dice img").forEach(function(die) {
                 die.classList.remove("roll");
             });
-        }, 300); // 1 second delay
 
-        // If player 1 wins
-        if (guestPlayer > computer) {
-            document.querySelector("h1").innerHTML = "<span style='color: green;'>🚩 Guest Player Wins!</span>";
-        } else if (computer > guestPlayer) {
-            document.querySelector("h1").innerHTML = "<span style='color: red;'>Computer Wins! 🚩</span>";
-    
-        } else {
-            document.querySelector("h1").innerHTML = "Draw!";
-        }
-    }, 100); // 100 millisecond delay for the animation to start
+            // If player 1 wins
+            if (guestPlayer > computer) {
+                document.querySelector("h1").innerHTML = "<span style='color: green;'>🚩 Guest Player Wins!</span>";
+            } else if (computer > guestPlayer) {
+                document.querySelector("h1").innerHTML = "<span style='color: red;'>Computer Wins! 🚩</span>";
+
+            } else {
+                document.querySelector("h1").innerHTML = "Draw!";
+            }
+        }, 200); // 200 millisecond delay after animation completes
+    }, 100); // 900 millisecond delay for the animation to start
 });
