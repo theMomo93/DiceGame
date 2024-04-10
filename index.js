743d544362534534b3a83c08701d0fb2f94f4dcb
theMomo93 <mkaminska93@gmail.com>
2024-04-10 10:53:44 +0200

    dice roll

--- index.js
+++ index.js
@@ -6,23 +6,38 @@ var image1 = document.querySelectorAll("img")[0];
 image1.setAttribute("src", randomImageSource);
 
 document.getElementById("button").addEventListener("click", function() {
-    var guestPlayer = Math.floor((Math.random() * 6) + 1); // 1-6
-    var randomDiceImage = "dice" + guestPlayer + ".png"; // dice1.png - dice6.png
-    var randomImageSource = "dice/" + randomDiceImage; // images/dice1.png - images/dice6.png
-    var image1 = document.querySelectorAll("img")[0];
+    // Add roll class to dice images
+    document.querySelectorAll(".dice img").forEach(function(die) {
+        die.classList.add("roll");
+    });
 
-    image1.setAttribute("src", randomImageSource);
+    setTimeout(function() {
+        var guestPlayer = Math.floor((Math.random() * 6) + 1);
+        var randomDiceImage = "dice" + guestPlayer + ".png";
+        var randomImageSource = "dice/" + randomDiceImage;
+        var image1 = document.querySelectorAll("img")[0];
 
-    var computer = Math.floor((Math.random() * 6) + 1);
-    var randomImageSource2 = "dice/dice" + computer + ".png";
-    document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+        image1.setAttribute("src", randomImageSource);
 
-    // If player 1 wins
-    if (guestPlayer > computer) {
-        document.querySelector("h1").innerHTML = "🚩 Guest Player Wins!";
-    } else if (computer > guestPlayer) {
-        document.querySelector("h1").innerHTML = "Computer Wins! 🚩";
-    } else {
-        document.querySelector("h1").innerHTML = "Draw!";
-    }
+        var computer = Math.floor((Math.random() * 6) + 1);
+        var randomImageSource2 = "dice/dice" + computer + ".png";
+        document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+        // Remove roll class after a delay to stop the animation
+        setTimeout(function() {
+            document.querySelectorAll(".dice img").forEach(function(die) {
+                die.classList.remove("roll");
+            });
+        }, 300); // 1 second delay
+
+        // If player 1 wins
+        if (guestPlayer > computer) {
+            document.querySelector("h1").innerHTML = "<span style='color: green;'>🚩 Guest Player Wins!</span>";
+        } else if (computer > guestPlayer) {
+            document.querySelector("h1").innerHTML = "<span style='color: red;'>Computer Wins! 🚩</span>";
+    
+        } else {
+            document.querySelector("h1").innerHTML = "Draw!";
+        }
+    }, 100); // 100 millisecond delay for the animation to start
 });
